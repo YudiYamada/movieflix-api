dcf9863cb07ab34698926c23eab4a7fcb90467bb
Yudi Yamada <yudiyamada10@gmail.com>
2025-06-25 15:01:29 -0300

    adicionada a mensagem de erro na criação do filme

--- src/server.ts
+++ src/server.ts
@@ -5,9 +5,41 @@ const port = 3000;
 const app = express();
 const prisma = new PrismaClient();
 
-app.get("/movies", async (req, res) => {
-  const movies = await prisma.movie.findMany();
+app.use(express.json());
+
+app.get("/movies", async (_, res) => {
+  const movies = await prisma.movie.findMany({
+    orderBy: {
+      title: "asc",
+    },
+    include: {
+      genres: true,
+      languages: true,
+    },
+  });
   res.json(movies);
+});
+
+app.post("/movies", async (req, res) => {
+  const { title, genre_id, language_id, oscar_count, release_date } = req.body;
+
+  try {
+    await prisma.movie.create({
+      data: {
+        title,
+        genre_id,
+        language_id,
+        oscar_count,
+        release_date: new Date(release_date),
+      },
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .send({ message: "Falha ao cadastrar um filme", error });
+  }
+
+  res.status(201).send();
 });
 
 app.listen(port, () => {
