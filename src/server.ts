3551130d65c9ee47ec7ae3404f00262e183520a3
Yudi Yamada <yudiyamada10@gmail.com>
2025-07-02 16:36:50 -0300

    adicionado a remoção de filmes

--- src/server.ts
+++ src/server.ts
@@ -54,6 +54,52 @@ app.post("/movies", async (req, res) => {
   res.status(201).send();
 });
 
+app.put("/movies/:id", async (req, res) => {
+  const id = Number(req.params.id);
+
+  try {
+    const movie = await prisma.movie.findUnique({
+      where: { id },
+    });
+
+    if (!movie) {
+      return res.status(404).send({ message: "Filme não encontrado" });
+    }
+
+    const data = { ...req.body };
+    data.release_date = data.release_date
+      ? new Date(data.release_date)
+      : undefined;
+
+    await prisma.movie.update({ where: { id }, data });
+  } catch (error) {
+    return res
+      .status(500)
+      .send({ message: "Falha ao atualizar o registro", error });
+  }
+  res.status(200).send();
+});
+
+app.delete("/movies/:id", async (req, res) => {
+  const id = Number(req.params.id);
+
+  try {
+    const movie = await prisma.movie.findUnique({
+      where: { id },
+    });
+
+    if (!movie) {
+      return res.status(404).send({ message: "Filme não encontrado" });
+    }
+
+    await prisma.movie.delete({ where: { id } });
+  } catch (error) {
+    res.status(500).send({ message: "Falha ao remover o registro", error });
+  }
+
+  res.status(200).send();
+});
+
 app.listen(port, () => {
   console.log(`Servidor em execução em http://localhost:${port}`);
 });
